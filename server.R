9dfa6be46c987aeb7e088be47b9a35131d857d1b
Rhonda Bacher <rhonda.bacher@gmail.com>
2017-08-01 15:20:23 -0500

    first commit

---
+++ server.R
@@ -0,0 +1,170 @@
+options(shiny.maxRequestSize=500*1024^2) 
+library(Trendy)
+library(EBSeq)
+library(parallel)
+library(segmented)
+library(shiny)
+library(shinyFiles)
+
+
+shinyServer(function(input, output, session) {
+  volumes <- c('home'="~")
+  shinyDirChoose(input, 'Outdir', roots=volumes, session=session, restrictions=system.file(package='base'))
+  output$Dir <- renderPrint({parseDirPath(volumes, input$Outdir)})
+  
+  wcinfo = sessionInfo(package="Trendy")
+  wcinfo_print = wcinfo$otherPkgs
+  
+  In <- reactive({
+    
+  
+    outdir <- paste0("~", do.call("file.path", input$Outdir[[1]]), "/")
+
+   
+    
+    withProgress(message = 'Progress:', value = 0, {
+      
+       incProgress(0.2, detail = "Uploading files...")
+       the.file <- input$filename
+       if (is.null(the.file)) { stop("Please upload data") }
+     
+       Sep=strsplit(the.file$name,split=".", fixed=TRUE)[[1]]
+    	  if(Sep[length(Sep)] == "csv") {
+          a1 = read.table(input$filename$datapath, stringsAsFactors = FALSE,
+                          header = TRUE, row.names = 1, comment.char = "", sep=",")
+        }                        
+        if(Sep[length(Sep)] != "csv") {
+            try((a1 = read.table(input$filename$datapath, stringsAsFactors = FALSE,
+                  header = TRUE, row.names = 1, comment.char = "", sep="\t")), silent=TRUE)
+      	    if(methods::is(a1, "try-error")) {
+      		    print("Initial data import failed, file format may be incorrect. Check that gene names are in the first 
+               row and that you have column names in your file.") 
+            }
+        }
+      
+     
+      
+        Data = data.matrix(a1)
+        time.file <- NULL
+        time.file <- input$TimeVector$name
+        if (is.null(time.file)) {
+            t.vect = seq_len(ncol(Data))}
+
+        if (!is.null(time.file)) {
+            Time.Sep = strsplit(time.file, split = ".", fixed=TRUE)[[1]]
+            if (Time.Sep[length(Time.Sep)] == "csv") {
+                TimeIn = read.table(input$TimeVector$datapath, stringsAsFactors = FALSE,
+                                    header = FALSE, sep=",")
+            }
+            if (Time.Sep[length(Time.Sep)] != "csv") {
+                TimeIn = read.table(input$TimeVector$datapath, stringsAsFactors = FALSE,
+                                header = FALSE, sep="\t")
+            }
+            t.vect = TimeIn[[1]]
+            names(t.vect) <- colnames(Data)
+            if (length(t.vect) != ncol(Data)) {
+              stop("Length of the time vector is not the same as the number of samples!") }
+
+        }
+
+       incProgress(0.1, detail = "Upload successful!")
+       
+       
+        List <- list(
+              InFile=the.file,
+              TimeFile=time.file,
+              NormTF = ifelse(input$Norm_buttons == "1", TRUE, FALSE), 
+        	    ScaleTF = ifelse(input$Scale_InData == "1", TRUE, FALSE), 
+              MeanCut = input$meanCut,
+              MaxK = input$maxK,
+              MinNumInSeg = input$minNum,
+              PvalCut = input$pvalCut,
+              CutDiff = input$cutDiff,
+              outputName = paste0(outdir, input$outName),
+              PlotData = ifelse(input$Plot_Data == "1", TRUE, FALSE),
+              Info = paste0(outdir, input$outName, "_run_parameters.txt"),
+              OutDir = outdir
+            )
+       incProgress(0.2, detail = "Formatting data for Trendy...")  
+    
+       
+        if (List$NormTF) {
+            Sizes <- EBSeq::MedianNorm(Data)
+            if (is.na(Sizes[1])) {
+              Sizes <- MedianNorm(Data, alternative=TRUE)
+              message("Alternative normalization method is applied.")
+            }
+            Data <- GetNormalizedMat(Data, Sizes)
+        }
+        if (List$ScaleTF) {
+            Data <- Data[which(rowMeans(Data) >= List$MeanCut), ]
+            Data <- t(apply(Data, MARGIN = 1, FUN = function(X) (X - min(X))/diff(range(X))))
+            List$MeanCut <- -Inf
+        }
+        
+        incProgress(0.4, detail = "Running Trendy...")
+
+        setwd(outdir)
+        seg.all <- trendy(Data[1:10,], Mean.Cut = List$MeanCut, Max.K = List$MaxK, T.Vect = t.vect,
+                        Min.Num.In.Seg = List$MinNumInSeg, Pval.Cut = List$PvalCut, 
+                        Cut.Diff = List$CutDiff, Save.Object = TRUE,
+                        File.Name = List$outputName)
+    
+       
+        getAll <- toptrendy(seg.all, AdjR.Cut = -Inf)
+
+        max.len <- max(sapply(getAll$slp, length))
+        corrected.list <- lapply(getAll$slp, function(x) {c(x, rep(NA, max.len - length(x)))})
+        slp.mat <- do.call(rbind, corrected.list)
+        colnames(slp.mat) <- paste0(seq_len(max.len))
+       
+
+        max.len <- max(sapply(getAll$bp, length))
+        corrected.list <- lapply(getAll$bp, function(x) {c(x, rep(NA, max.len - length(x)))})
+        bp.mat <- do.call(rbind, corrected.list)
+        colnames(bp.mat) <- paste0(seq_len(max.len))
+
+        getRsq <- sapply(seg.all, function(x) x$radj)
+        GenesToPlot <- names(sort(getRsq, decreasing=TRUE))
+        toOut <- data.frame(Gene = GenesToPlot, Slopes = round(slp.mat[GenesToPlot,], 5), 
+              Breakpoints = round(bp.mat[GenesToPlot,],5), adjustedRsq = round(getRsq[GenesToPlot],5))
+
+     if (List$PlotData) {
+        incProgress(0.1, detail = "Making scatterplots...")
+        XX <- plotmarker(Data, T.Vect = t.vect, File.Name = paste0(List$outputName,"_scatter.pdf"),
+                      Feature.Names=GenesToPlot, PDF= TRUE,
+                      Seg.Data = seg.all)
+      }
+
+      write.table(toOut, file=paste0(List$outputName, "_summaryTrendy.csv"), quote=F, sep=",", row.names=FALSE)
+        })
+    ## Sessioninfo & input parameters
+    sink(List$Info)
+    print(paste0("Package version: ", wcinfo_print$Trendy$Version))
+    print("Input parameters:")
+    print(paste0("Input File: ", List$InFile[1]))
+    print(paste0("Time File: ", List$TimeFile[1]))
+    print(paste0("Whether to normalize? ", List$NormTF))
+    print(paste0("Whether to scale? ", List$ScaleTF))
+    print(paste0("Mean cutoff: ", List$MeanCut))
+    print(paste0("Maximum possible breakpoints considered: ", List$MaxK))
+    print(paste0("Minimum number of data per segment: ", List$MinNumInSeg))
+    print(paste0("P-value for significance of segment trend (up or down): ", List$PvalCut))
+    print(paste0("Increase in model fit required to use additional breakpoints ", List$CutDiff))
+    sink()
+    
+    
+    List=c(List)	
+})   
+  
+  Act <- eventReactive(input$Submit,{
+    In()})
+  # Show the values using an HTML table
+  output$print0 <- renderText({
+    tmp <- Act()
+    print("Done!")
+  })
+  
+
+
+})
---
+++ ui.R
@@ -0,0 +1,86 @@
+library(shiny)
+library(shinyFiles)
+options(shiny.maxRequestSize=10000*1024^2) 
+
+shinyUI(fluidPage(
+  #  Application title
+  headerPanel("Trendy"),
+  
+  # Sidebar with sliders that demonstrate various available options
+  fluidRow(width = 12, height = 100,
+               # file
+              column(4,
+               fileInput("filename", label = "Data file input (support .csv, .txt, .tab)"),
+               
+               # time vector
+               fileInput("TimeVector", label = "Time vector \n file name (e.g. support .csv, .txt, .tab)"),
+               
+              
+                 # Normalization
+                 radioButtons("Norm_buttons",
+                              label = "Do you need to normalize the data?",
+                              choices = list("Yes" = 1,
+                                             "No" = 2),
+                              selected = 2),
+
+                # Scale
+                 radioButtons("Scale_InData",
+                           label = "Center and scale the data?",
+                           choices = list("Yes" = 1,
+                                          "No" = 2),
+                           selected = 2),
+                          
+             radioButtons("Plot_Data",
+                       label = "Output scatterplots with Trendy fit (sorted by adjusted R^2)?",
+                       choices = list("Yes" = 1,
+                                      "No" = 2),
+                       selected = 1)
+                       ),            
+              column(4,
+                  numericInput("meanCut",
+                              label = "Only consider genes with mean greater than",
+                              value = 10),
+                 # MaxK
+                 numericInput("maxK",
+                              label = "Maximum number of breakpoints",
+                              value = 5),
+                 # MinNum
+                 numericInput("minNum",
+                              label = "Minimum number of data points in each segment",
+                              value = 3),         
+                     
+                 # pvalcut
+                 numericInput("pvalCut",
+                              label = "P-value to determine direction of segment (up, down, or same)",
+                              value = .2),
+                 # CutDiff
+                 numericInput("cutDiff",
+                              label = "Improvement in model fit (Rsquared) to use additional breakpoints",
+                              value = .1),
+
+                 # out name
+                 textInput("outName", 
+                           label = "Name of output files", 
+                           value = "trendy_results")
+                      
+                
+               ),
+               
+               column(4,
+                      # output dir
+                      tags$div(tags$b("Please select a folder for output :")),
+                      
+                      shinyDirButton('Outdir', label ='Select Output Folder', title = 'Please select a folder'),
+                      tags$br(),
+                      tags$br()
+                      ),
+               br(),
+               br(),
+               actionButton("Submit","Submit for processing")
+  ),
+  
+  # Show a table summarizing the values entered
+  mainPanel(
+    h4(textOutput("print0"))
+  )
+))
